e34492093deee82ffabd296f472aedaa76833e0b
Eugene <eugklim1@gmail.com>
2024-11-08 17:30:44 +0300

    first commit 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Main page</h1>
+      <h1>Main page 1231241242314</h1>
 
       <div>
         <h1>Главная страница</h1>
